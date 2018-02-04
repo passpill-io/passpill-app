5069d2a8ffc44ff87ad6a7db59e1294360a51b6a
Javier Marquez <javi@arqex.com>
2018-02-04 17:37:55 +0100

    Deactivates live version properly

--- src/js/boot.js
+++ src/js/boot.js
@@ -2,9 +2,6 @@ import { render } from 'preact';
 import '../scss/all.scss';
 import 'state/reactions';
 
-if( location.host.indexOf('passpill.io') !== -1 && location.search.indexOf('dev') === -1 ){
-	return (document.body.innerHTML = 'Coming soon');
-}
 
 function init() {
 	let elem = document.getElementById('root'),
@@ -14,17 +11,20 @@ function init() {
 	elem = render(App, elem);
 }
 
-init();
-
-if (process.env.NODE_ENV === 'production') {
-	// cache all assets if browser supports serviceworker
-	if ('serviceWorker' in navigator && location.protocol === 'https:') {
-		navigator.serviceWorker.register('/sw.js');
-	}
-
-} else {
-	// listen for HMR
-	if (module.hot) {
-		module.hot.accept('./App', init);
+if( location.host.indexOf('passpill.io') !== -1 && location.search.indexOf('dev') === -1 ){
+	document.body.innerHTML = 'Coming soon';
+}
+else {
+	init();
+	if (process.env.NODE_ENV === 'production') {
+		// cache all assets if browser supports serviceworker
+		if ('serviceWorker' in navigator && location.protocol === 'https:') {
+			navigator.serviceWorker.register('/sw.js');
+		}
+	} else {
+		// listen for HMR
+		if (module.hot) {
+			module.hot.accept('./App', init);
+		}
 	}
 }
