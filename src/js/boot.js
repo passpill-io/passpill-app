d028b702eec619e3d7d997b90f38d883f3d734e2
Javier Marquez <javi@arqex.com>
2018-02-24 17:19:25 +0100

    Updates styles and removes the dev restriction

--- src/js/boot.js
+++ src/js/boot.js
@@ -12,20 +12,15 @@ function init() {
 	ReactDOM.render(App, elem);
 }
 
-if( location.host.indexOf('passpill.io') !== -1 && location.search.indexOf('dev') === -1 ){
-	document.body.innerHTML = 'Coming soon';
-}
-else {
-	init();
-	if (process.env.NODE_ENV === 'production') {
-		// cache all assets if browser supports serviceworker
-		if ('serviceWorker' in navigator && location.protocol === 'https:') {
-			navigator.serviceWorker.register('/sw.js');
-		}
-	} else {
-		// listen for HMR
-		if (module.hot) {
-			module.hot.accept('./App', init);
-		}
+init();
+if (process.env.NODE_ENV === 'production') {
+	// cache all assets if browser supports serviceworker
+	if ('serviceWorker' in navigator && location.protocol === 'https:') {
+		navigator.serviceWorker.register('/sw.js');
+	}
+} else {
+	// listen for HMR
+	if (module.hot) {
+		module.hot.accept('./App', init);
 	}
 }
